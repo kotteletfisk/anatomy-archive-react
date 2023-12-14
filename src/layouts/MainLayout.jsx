d382ee5cd21ba4558fb921e66f58610ad8d2ae80
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-12-14 11:23:32 +0100

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,25 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 
 function MainLayout() {
+  const location = useLocation();
+
+  function showContent() {
+    if (location.pathname === "/") {
+      return (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto,
+            voluptatibus non! Minus, expedita! Aliquam illo, odio repellat
+            temporibus, voluptates nobis sunt minus dolore a delectus assumenda
+            minima, corrupti earum consequatur.
+          </p>
+        </>
+      );
+    } else {
+      return <Outlet />;
+    }
+  }
+
   return (
     <>
       <h1>Main Layout</h1>
@@ -12,9 +31,7 @@ function MainLayout() {
         </div>
       </div>
 
-      <div id="content">
-        <Outlet />
-      </div>
+      <div id="content">{showContent()}</div>
     </>
   );
 }
