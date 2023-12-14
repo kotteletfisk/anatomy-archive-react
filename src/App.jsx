e034cd43b2e8350b5f4f5055eabbf249c4120b4b
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-12-14 11:08:06 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import Test from "./pages/Test";
 
 function App() {
   const routes = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
-        {/*  */}
+        <Route path="/test" element={<Test />} />
       </Route>
     )
   );
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,20 @@
+import { NavLink, Outlet } from "react-router-dom";
+
 function MainLayout() {
   return (
     <>
       <h1>Main Layout</h1>
+
+      <div id="nav-bar">
+        <div id="nav-items">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/test">Test</NavLink>
+        </div>
+      </div>
+
+      <div id="content">
+        <Outlet />
+      </div>
     </>
   );
 }
