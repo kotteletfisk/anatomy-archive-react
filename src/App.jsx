bf242fd15f66b0f29bcd0f38633d9c6d1619d4cd
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-12-14 10:32:21 +0100

    made package structur and start route for root

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import "./styles/App.css";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        {/*  */}
+      </Route>
+    )
+  );
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-          <img src="https://i.imgur.com/YcP0tik.jpeg" alt="Testerino" />
-              </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={routes} />;
 }
 
-export default App
+export default App;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,9 @@
+function MainLayout() {
+  return (
+    <>
+      <h1>Main Layout</h1>
+    </>
+  );
+}
+
+export default MainLayout;
