4681c8227bdf2cd895b700b3fb07c8166694f56f
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-12-15 11:26:38 +0100

    update homepage

---
+++ src/components/WeeklyStatus.jsx
@@ -0,0 +1,15 @@
+function WeeklyStatus() {
+  return (
+    <>
+      <h3>Weekly status : </h3>
+
+      <div id="wrapper-status">
+        <div>Visitors : 9000 </div>
+        <div>Subscribers : 100 </div>
+        <div>Reviews : 100 </div>
+      </div>
+    </>
+  );
+}
+
+export default WeeklyStatus;
