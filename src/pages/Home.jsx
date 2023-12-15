91977f26ba1388dd6de388971bf89ccbf89c901b
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-12-15 11:41:39 +0100

    change quot layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,12 @@ function Home() {
     <div>
       <h1>Home</h1>
 
-      <i>
+      <i id="homepage-quot">
         &quot;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto,
         voluptatibus non! Minus, expedita! Aliquam illo, odio repellat
         temporibus, voluptates nobis sunt minus dolore a delectus assumenda
-        minima, corrupti earum consequatur&quot; - Founders of Anatomy Archive
+        minima, corrupti earum consequatur&quot; <br /> - Founders of Anatomy
+        Archive
       </i>
 
       <WeeklyStatus />
