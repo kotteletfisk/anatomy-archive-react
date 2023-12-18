b0c27614fd20234a7a0a5503a39abfb14be91533
kotteletfisk <ostekugle@gmail.com>
2023-12-18 19:40:50 +0100

    testing svg

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Muscleman from './Muscleman'
 import Muscleman2 from './Muscleman2'
+import Muscleman3 from '/male-anatomy-edit-1.svg'
+// import Muscleman4 from '/Anatomy-both.svg'
+import Muscleman4 from './Muscleman4'
 import './App.css'
 
 function App() {
@@ -11,7 +14,9 @@ function App() {
     <>
       <div>
         <h1>React + Vite</h1>
-        <Muscleman2 />
+        {/* <Muscleman2 /> */}
+        <img src={Muscleman4} alt="NO MAN" />
+        <img src={Muscleman3} alt="NO MAN" />
       </div>
     </>
   )
