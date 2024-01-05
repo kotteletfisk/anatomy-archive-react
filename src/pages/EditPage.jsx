a2d91e01055edf1b2823ffa3e0e129297e1fa2ec
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2024-01-05 11:48:01 +0100

    Update EditPage.jsx

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { crud } from "../util/facade";
 import ExerciseEditForm from "../components/ExerciseEditForm";
@@ -60,6 +60,5 @@ function EditPage() {
     </>
   );
 }
-
 
 export default EditPage;
