de84198184e5741a68b994b0f49b6d8a6b29d4d2
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2024-01-05 11:36:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,22 @@ import LoginModal from "./components/LoginModal";
 import DetailComponent from "./pages/DetailComponent";
 import AdminPage from "./pages/AdminPage";
 import EditPage from "./pages/EditPage";
+import { useContext } from "react";
+import AuthContext from "./components/AuthContext";
 
 function App() {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
   const routes = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />} errorElement={<ErrorPage />}>
         <Route path="/search" element={<Search />} />
         <Route path="/login" element={<LoginModal />} />
-        <Route path="/admin-page" element={<AdminPage />} />
+
+        {isLoggedIn ? (
+          <Route path="/admin-page" element={<AdminPage />} />
+        ) : null}
+
         <Route path="/exercises/:id" element={<DetailComponent />} />
         <Route path="/EditPage/:entity/:id" element={<EditPage />} />
       </Route>
