8162c75649ae66c95821f171c31414948df369ce
kotteletfisk <ostekugle@gmail.com>
2024-01-18 00:20:52 +0100

    updated svg component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Muscleman from './Muscleman'
-import Muscleman2 from './Muscleman2'
-import Muscleman3 from '/male-anatomy-edit-1.svg'
+import SvgComponent from './SvgComponent'
+// import Muscleman3 from '/male-anatomy-edit-1.svg'
 // import Muscleman4 from '/Anatomy-both.svg'
-import Muscleman4 from './Muscleman4'
+// import Muscleman4 from './Muscleman4'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>React + Vite</h1>
-        {/* <Muscleman2 /> */}
-        <img src={Muscleman4} alt="NO MAN" />
-        <img src={Muscleman3} alt="NO MAN" />
+      <div className='container'>
+        <h1>Hello</h1>
+        <SvgComponent />
       </div>
     </>
   )
